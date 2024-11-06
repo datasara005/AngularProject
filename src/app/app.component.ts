bca6bf957e68762463d9e5275875cfd1eda8688b
Akaki Tsulukidze <AkakiTsulukidze@credo.ge>
2024-11-06 13:57:58 +0400

    fix http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InformaciaService } from './service/informacia.service';
 import { InformaciuliModel } from './models/informaciuli.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
